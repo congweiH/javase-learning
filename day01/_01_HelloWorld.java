3d8ff84929759466fb0c74671018d52ae3c2351c
looeyWei <congwei_huang@163.com>
2021-03-06 21:56:53 +0800

    完成day01

---
+++ day01/_01_HelloWorld.java
@@ -0,0 +1,23 @@
+
+/*
+jdk的作用:
+    jre是用来运行java程序的
+
+jdk,jre,jvm的关系:
+    jdk里面有jre, jre里面有jvm
+
+*/
+
+// 类名 和 文件名相同
+
+/**
+ *文档注释
+ * 这是一个打印hello world的类
+ * @author looeyWei
+ * @version 1.0.0
+ */
+public class _01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ day01/_02_作业.java
@@ -0,0 +1,26 @@
+/**
+ * 课后作业
+ * @author looeyWei
+ */
+public class _02_作业 {
+    public static void main(String[] args) {
+        // 1. 打印 Hellojava
+        System.out.println("Hellojava");
+
+        // 2. 输出个人信息
+        System.out.println("looeyWei");
+        System.out.println("男");
+        System.out.println("江西");
+        System.out.println("上饶");
+
+        // 3. 打印字符图形
+        System.out.println("    *                        * ");
+        System.out.println("  *    *    I love Java  *      *");
+        System.out.println("   *                         *");
+        System.out.println("     *                     *");
+        System.out.println("       *                 *");
+        System.out.println("          *           *");
+        System.out.println("             *     *");
+        System.out.println("                *   ");
+    }
+}
